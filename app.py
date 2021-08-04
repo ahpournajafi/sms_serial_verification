19a99a5ef2160df0734a44bda718417ea31d090b
ahpournajafi <ahpournajafi@gmail.com>
2021-08-04 10:14:19 +0430

    reading data from excel file

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
+from pandas import read_excel
 import requests
 import config
-
+app=Flask(__name__)
 # @app.route('/')
 # def main_page():
 #     '''
@@ -30,6 +31,18 @@ def send_sms(message, receptor):
     }
     r = requests.post(url, data)
     print(f'message *{message}* sent . status code is {r.status_code}')
+
+def import_database_from_excel(filepath):
+    ''' gets an excel filename and import lookup data (data and failures) frok it '''
+    # df contain lookup data in the form of
+    # Reference Number	Description	Start Serial	End Serial	Date
+    df = read_excel(filepath, 0) 
+    for index,(line,ref,  desc, start_serial, end_serial, date) in df.iterrows():
+        print(line,ref,  desc, start_serial, end_serial, date)
+    df = read_excel(filepath, 1) # sheet one contains failed serial number . only one column .
+    for index, i in df.iterrows():
+        print(i[0])
+
 def check_serial():
     pass
 
@@ -37,3 +50,4 @@ def check_serial():
 if __name__=="__main__":
     # send_sms('Hi There', '09375546640') جهت تست برنامه که نیازی به ران کردن هم نیست
     # app.run("127.0.0.1" , 5000, debug=True)
+    import_database_from_excel('../../../data.xlsx')
